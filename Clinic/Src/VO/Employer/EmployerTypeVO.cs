25c0464c49207e364586b75d54fa7baa6c5d0924
fil7er <fil7er@users.noreply.github.com>
2022-03-30 08:29:53 -0300

    add entities

---
+++ Clinic/Src/VO/Employer/EmployerTypeVO.cs
@@ -0,0 +1,8 @@
+﻿namespace Clinic.Src.VO.Employer
+{
+    public class EmployerTypeVO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
---
+++ Clinic/Src/VO/Employer/EmployerVO.cs
@@ -0,0 +1,30 @@
+﻿namespace Clinic.Src.VO.Employer
+{
+    public class EmployerVO
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; } 
+        public int Enrollment { get; set; }
+        public string? CPF { get; set; }
+        public string? RG { get; set; }
+        private EmployerTypeVO employerType;
+
+        public EmployerVO(EmployerTypeVO employerType)
+        {
+            this.employerType = employerType;
+        }
+
+        public EmployerTypeVO EmployerType 
+        { 
+            get 
+            { 
+                return this.employerType; 
+            } 
+            set 
+            {
+
+                this.employerType = value; 
+            }
+        }
+    }
+}
